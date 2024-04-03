f240980d6fd85cb0709f61b9984bfea44aed8363
argemiro_oliveira <140517194+argewar@users.noreply.github.com>
2024-04-03 13:27:17 -0300

    atualizadonovamente

---
+++ Sist_Especialista/ExpertSystemVSC/appVeiculoMain.py
@@ -0,0 +1,22 @@
+from esMenu import APP
+from appVehicleRuleBase import RuleBaseVehicle
+
+class Main:
+    def __init__(self):
+        self.app = APP("Rule Application")
+
+    def main(self):
+        try:
+            # RuleBaseVehicle recebe dois parâmetros:
+            # o primeiro é nome da base de regras e
+            # o segundo é lista de várias presentes na base de regras.
+            # Essas variáveis fazem parte dos possíveis objetivos
+            br_vehicle = RuleBaseVehicle("Veiculos Rule Base", "[veiculo] :")
+            self.app.add_rule_base(br_vehicle)
+            self.app.menu()
+        except Exception as e:
+            print("Exception: RuleApp ", e.with_traceback())
+
+
+if __name__ == '__main__':
+    Main().main()
